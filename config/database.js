5f1ae4c074a09286d4b9d372bc7ad71903a3c757
LazerTorpedo <52832125+LazerTorpedo@users.noreply.github.com>
2021-03-26 20:57:27 +0530

    hide db info

--- config/database.js
+++ config/database.js
@@ -1,10 +1,10 @@
 const mysql=require('mysql');
 const db=mysql.createConnection({
-    user:'um1pqho1f9nxnzt6',
-    password:'dG7FyGtBRaP0tDYDDaou',
-    host:'b8gryulv6zsavzfzyqrs-mysql.services.clever-cloud.com',
+    user:'?',
+    password:'?',
+    host:'?s-mysql.services.clever-cloud.com',
     port:'3306',
-    database:'b8gryulv6zsavzfzyqrs',
+    database:'?',
     multipleStatements:true
 });
 db.connect((err)=>{
